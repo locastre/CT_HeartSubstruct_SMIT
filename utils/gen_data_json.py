a7622cc2b82771c41d8fccdf48b73e521df690f3
locastre <34068528+locastre@users.noreply.github.com>
2024-10-17 00:10:33 -0400

    create json for multiple nii in folder

--- utils/gen_data_json.py
+++ utils/gen_data_json.py
@@ -1,25 +1,16 @@
 import os, json, sys
+from glob import glob
 
-
-data_file_path = sys.argv[1]
-
-data_dir = os.path.dirname(data_file_path)
-nii_file = os.path.basename(data_file_path)
-
+data_dir = sys.argv[1]
+nii_filelist = glob(os.path.join(data_dir,'*.nii*'))
 
 out_json = os.path.join(data_dir,'data.json')
 
-data_json = {
-  "val": 
-  [
-    {
-      "image": nii_file
-    }
-	
-  ]
-}
+data_json = {'val':[]}
 
-
+for nii_file in nii_filelist:
+    nii_base = os.path.basename(nii_file)
+    data_json['val'].append({'image':nii_base})
 
 json_object = json.dumps(data_json, indent=4)
  
